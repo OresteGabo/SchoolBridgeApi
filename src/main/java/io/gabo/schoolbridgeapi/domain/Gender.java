e8c48b5f1ff741d457cab50c28d35c711a09d1ea
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:39:44 +0200

    Add Gender enum definition

---
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Gender.java
@@ -0,0 +1,10 @@
+package io.gabo.schoolbridgeapi.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
+
