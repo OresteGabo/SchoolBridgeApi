1eddc475989b4b04e098e853da92c587b6803972
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:30:10 +0200

    Refactor Sector entity: remove `@Data` annotation, add explicit getter/setter methods, and clean up imports

--- src/main/java/io/gabo/schoolbridgeapi/domain/Sector.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Sector.java
@@ -1,14 +1,8 @@
 package io.gabo.schoolbridgeapi.domain;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.util.HashSet;
-import java.util.Set;
-@Entity(name = "sectors") @Data @NoArgsConstructor
+@Entity(name = "sectors") @NoArgsConstructor
 public class Sector {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,4 +11,28 @@ public class Sector {
 
     @ManyToOne(optional = false)
     private District district;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
 }
