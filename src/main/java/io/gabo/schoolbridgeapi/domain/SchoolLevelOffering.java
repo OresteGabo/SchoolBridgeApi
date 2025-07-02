1da49971fd20fdcb33b531c8954fa324c2a2424a
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 11:09:51 +0200

    Refactor SchoolLevelOffering entity formatting for improved readability and consistency

--- src/main/java/io/gabo/schoolbridgeapi/domain/SchoolLevelOffering.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/SchoolLevelOffering.java
@@ -10,14 +10,14 @@ import java.time.OffsetDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity(name = "school_level_offerings") @Data @NoArgsConstructor
-class SchoolLevelOffering {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @ManyToOne(optional = false) private School          school;
-    @ManyToOne(optional = false) private EducationLevel  educationLevel;
-    @ManyToOne(optional = false) private AcademicYear    academicYear;
+
+@Entity(name = "school_level_offerings")  @Data @NoArgsConstructor
+class SchoolLevelOffering {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @ManyToOne(optional = false) private School school;
+    @ManyToOne(optional = false) private EducationLevel educationLevel;
+    @ManyToOne(optional = false) private AcademicYear academicYear;
 
     @OneToMany(mappedBy = "offering", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<SchoolExtraCourse> extraCourses = new HashSet<>();
