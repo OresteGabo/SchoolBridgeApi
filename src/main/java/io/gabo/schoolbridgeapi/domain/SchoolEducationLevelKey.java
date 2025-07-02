4294b02acf73fac121e15d239f106ea63ba345a6
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:55:50 +0200

    Make SchoolEducationLevelKey class public and replace Lombok's @Data with explicit getter and setter methods

--- src/main/java/io/gabo/schoolbridgeapi/domain/SchoolEducationLevelKey.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/SchoolEducationLevelKey.java
@@ -4,8 +4,24 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Embeddable @Data @NoArgsConstructor
-class SchoolEducationLevelKey {
+@Embeddable @NoArgsConstructor
+public class SchoolEducationLevelKey {
     private Long schoolId;
     private Long educationLevelId;
+
+    public Long getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(Long schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public Long getEducationLevelId() {
+        return educationLevelId;
+    }
+
+    public void setEducationLevelId(Long educationLevelId) {
+        this.educationLevelId = educationLevelId;
+    }
 }
