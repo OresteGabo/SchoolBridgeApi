c6ecb3f3776b16d5e5837e2d6c3ec8bf000e004e
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 12:10:13 +0200

    Refactor Course entity to remove @Data, improve formatting, and add explicit getter/setter methods

--- src/main/java/io/gabo/schoolbridgeapi/domain/Course.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Course.java
@@ -1,7 +1,6 @@
 package io.gabo.schoolbridgeapi.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
@@ -9,12 +8,44 @@ import java.util.Set;
 
 
 
-@Entity (name ="courses") @Data @NoArgsConstructor
-class Course {
+@Entity (name ="courses")  @NoArgsConstructor
+public class Course {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     @Column(nullable = false, length = 50, unique = true) private String name;
     @Column(length = 250) private String description;
 
     // reverse link for clarity
     @ManyToMany(mappedBy = "mainCourses") private Set<Combination> combinations = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Combination> getCombinations() {
+        return combinations;
+    }
+
+    public void setCombinations(Set<Combination> combinations) {
+        this.combinations = combinations;
+    }
 }
