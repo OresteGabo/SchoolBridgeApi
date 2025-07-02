accf640378388188aeb8ecb4cd3275e214a89f25
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:41:14 +0200

    Add SchoolExtraCourseKey embeddable class definition

---
+++ src/main/java/io/gabo/schoolbridgeapi/domain/SchoolExtraCourseKey.java
@@ -0,0 +1,13 @@
+package io.gabo.schoolbridgeapi.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+class SchoolExtraCourseKey {
+    private Long schoolLevelOfferingId;
+    private Long courseId;
+}
