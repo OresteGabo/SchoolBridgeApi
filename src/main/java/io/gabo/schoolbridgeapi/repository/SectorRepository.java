758d3cf52defa8b94cc0e7aadbf9d254916116a1
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:41:55 +0200

    Add SectorRepository interface definition

---
+++ src/main/java/io/gabo/schoolbridgeapi/repository/SectorRepository.java
@@ -0,0 +1,14 @@
+package io.gabo.schoolbridgeapi.repository;
+
+import io.gabo.schoolbridgeapi.domain.Sector;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+/*
+public interface SectorRepository extends JpaRepository<Sector, String> {
+    Sector findByDistrictIdAndLocalId(String districtId, String localId);
+    boolean existsByDistrictIdAndLocalId(String districtId, String localId);
+    boolean existsById(String id);
+    boolean existsByDistrictId(String districtId);
+}
+*/
