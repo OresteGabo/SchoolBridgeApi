324d6e5b55feafd71f5f1396abf28eb15eeca30c
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:39:15 +0200

    Add DistrictRepository interface definition

---
+++ src/main/java/io/gabo/schoolbridgeapi/repository/DistrictRepository.java
@@ -0,0 +1,14 @@
+package io.gabo.schoolbridgeapi.repository;
+
+import io.gabo.schoolbridgeapi.domain.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+/*
+public interface DistrictRepository extends JpaRepository<District, String> {
+    District findByName(String name);
+    Optional<District> findById(String id);
+    boolean existsByName(String name);
+    boolean existsById(String id);
+}
+*/
