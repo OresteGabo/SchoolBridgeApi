79288ed8a715d4b56044dbf193840626e159b862
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:30:16 +0200

    Refactor SectorRepository: annotate with `@Repository`, update method signatures, and remove commented-out code

--- src/main/java/io/gabo/schoolbridgeapi/repository/SectorRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/SectorRepository.java
@@ -1,14 +1,14 @@
 package io.gabo.schoolbridgeapi.repository;
 
+import io.gabo.schoolbridgeapi.domain.District;
 import io.gabo.schoolbridgeapi.domain.Sector;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-/*
-public interface SectorRepository extends JpaRepository<Sector, String> {
-    Sector findByDistrictIdAndLocalId(String districtId, String localId);
-    boolean existsByDistrictIdAndLocalId(String districtId, String localId);
-    boolean existsById(String id);
-    boolean existsByDistrictId(String districtId);
+import java.util.List;
+
+@Repository
+public interface SectorRepository extends JpaRepository<Sector, Long> {
+    List<Sector> findByDistrictId(Long districtId);
+    List<Sector> findByDistrict(District district);
 }
-*/
