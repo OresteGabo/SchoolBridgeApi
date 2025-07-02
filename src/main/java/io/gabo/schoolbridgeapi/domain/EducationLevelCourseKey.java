32717b2d147a7e01eabff2a7b4eb5258cc523212
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 11:09:20 +0200

    Refactor EducationLevelCourseKey annotations for cleaner formatting

--- src/main/java/io/gabo/schoolbridgeapi/domain/EducationLevelCourseKey.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/EducationLevelCourseKey.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Embeddable
-@Data
-@NoArgsConstructor
+@Embeddable @Data @NoArgsConstructor
 class EducationLevelCourseKey {
     private Long educationLevelId;
     private Long courseId;
