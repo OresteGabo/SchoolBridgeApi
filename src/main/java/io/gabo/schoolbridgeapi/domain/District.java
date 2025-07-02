d688ac61d2fc2ef5e77882c5ceaf4b81f2a1b0fe
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:29:31 +0200

    Refactor District entity: remove `@Data` annotation, add explicit getter/setter methods, and annotate `sectors` field with `@JsonIgnore`.

--- src/main/java/io/gabo/schoolbridgeapi/domain/District.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/District.java
@@ -1,17 +1,13 @@
 package io.gabo.schoolbridgeapi.domain;
 
-import io.gabo.schoolbridgeapi.domain.Sector;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
+import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity(name = "districts") @Data @NoArgsConstructor
+@Entity(name = "districts") @NoArgsConstructor
 public class District {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,5 +15,31 @@ public class District {
     private String name;
 
     @OneToMany(mappedBy = "district", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private Set<Sector> sectors = new HashSet<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Sector> getSectors() {
+        return sectors;
+    }
+
+    public void setSectors(Set<Sector> sectors) {
+        this.sectors = sectors;
+    }
 }
