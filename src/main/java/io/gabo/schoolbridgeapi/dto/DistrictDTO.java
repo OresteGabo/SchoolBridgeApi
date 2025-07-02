f4074f7e9e76ac0c89a7012aead402cbf63daa04
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 16:48:07 +0200

    Add DistrictDTO with fields, constructors, and accessor methods

--- src/main/java/io/gabo/schoolbridgeapi/dto/DistrictDTO.java
+++ src/main/java/io/gabo/schoolbridgeapi/dto/DistrictDTO.java
@@ -5,9 +5,32 @@ public class DistrictDTO {
     private String name;
     private int sectorCount;
 
+    public DistrictDTO() {}
+
     public DistrictDTO(Long id, String name, int sectorCount) {
         this.id = id;
         this.name = name;
+        this.sectorCount = sectorCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSectorCount() {
+        return sectorCount;
+    }
+    public void setSectorCount(int sectorCount) {
         this.sectorCount = sectorCount;
     }
 }
