f0dda6f98a10c464e544946e92a1ef8219627691
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 11:09:37 +0200

    Refactor SchoolExtraCourseKey annotations for cleaner formatting

--- src/main/java/io/gabo/schoolbridgeapi/domain/SchoolExtraCourseKey.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/SchoolExtraCourseKey.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Embeddable
-@Data
-@NoArgsConstructor
+@Embeddable @Data @NoArgsConstructor
 class SchoolExtraCourseKey {
     private Long schoolLevelOfferingId;
     private Long courseId;
