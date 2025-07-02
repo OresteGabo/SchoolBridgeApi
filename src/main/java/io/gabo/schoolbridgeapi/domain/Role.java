7b5e1263815836747e5424666d902d3e19835fb7
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:55:37 +0200

    Replace Lombok's @Data with explicit getter and setter methods in Role class

--- src/main/java/io/gabo/schoolbridgeapi/domain/Role.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Role.java
@@ -2,16 +2,39 @@ package io.gabo.schoolbridgeapi.domain;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "roles")
-@Data @NoArgsConstructor
-class Role {
+@NoArgsConstructor
+public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, length = 50)
     private String description;
     @Column(nullable = false, length = 15, unique = true)
     private String dbName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
 }
