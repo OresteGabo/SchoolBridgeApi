8a9fed68fc45df06e41f70cbcb017ae184151dd1
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:30:04 +0200

    Refactor Section entity: remove `@Data` annotation, add explicit getter/setter methods, and annotate `combinations` field with `@JsonIgnore`.

--- src/main/java/io/gabo/schoolbridgeapi/domain/Section.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Section.java
@@ -1,20 +1,51 @@
 package io.gabo.schoolbridgeapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 
-@Entity(name = "sections") @Data @NoArgsConstructor
-class Section {
+@Entity(name = "sections")  @NoArgsConstructor
+public class Section {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     @Column(nullable = false, length = 50) private String name;  // Science, Arts, Commerce
 
     @ManyToOne(optional = false) private EducationLevel educationLevel;
 
     @OneToMany(mappedBy = "section")
+    @JsonIgnore
     private Set<Combination> combinations = new HashSet<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EducationLevel getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(EducationLevel educationLevel) {
+        this.educationLevel = educationLevel;
+    }
+
+    public Set<Combination> getCombinations() {
+        return combinations;
+    }
+
+    public void setCombinations(Set<Combination> combinations) {
+        this.combinations = combinations;
+    }
 }
