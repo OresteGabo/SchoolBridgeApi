9d580caff139690af7b6c8fd1226ac3263ee7c27
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 11:09:57 +0200

    Refactor Section entity formatting and annotations for improved readability and consistency

--- src/main/java/io/gabo/schoolbridgeapi/domain/Section.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Section.java
@@ -8,23 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-@Entity(name = "sections")
-class Section {
-    @Id
-    @GeneratedValue
-    Long id;
-
-    @Column
-    String name;  // e.g., Science, Arts, Commerce
-
-    @ManyToOne(optional = false)
-    SchoolLevel schoolLevel;
-}
-
-/*
-
-@Entity @Data
-@NoArgsConstructor
+@Entity(name = "sections") @Data @NoArgsConstructor
 class Section {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     @Column(nullable = false, length = 50) private String name;  // Science, Arts, Commerce
@@ -34,4 +18,3 @@ class Section {
     @OneToMany(mappedBy = "section")
     private Set<Combination> combinations = new HashSet<>();
 }
-*/
