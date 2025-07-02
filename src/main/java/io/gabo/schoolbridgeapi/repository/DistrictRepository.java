87f6f2f60935741708b28993b8e17d45a2564658
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 16:48:13 +0200

    Update DistrictRepository to return a list in findByName instead of an optional

--- src/main/java/io/gabo/schoolbridgeapi/repository/DistrictRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/DistrictRepository.java
@@ -4,9 +4,10 @@ import io.gabo.schoolbridgeapi.domain.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DistrictRepository extends JpaRepository<District, Long> {
-    Optional<District> findByName(String name);
+    List<District> findByName(String name);
 }
