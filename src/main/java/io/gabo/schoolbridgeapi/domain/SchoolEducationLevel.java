3d416dff84c90d4fa3ecff15fd755d64dcdda461
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 15:08:54 +0200

    Replace Lombok's @Data with explicit getter and setter methods in SchoolEducationLevel class

--- src/main/java/io/gabo/schoolbridgeapi/domain/SchoolEducationLevel.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/SchoolEducationLevel.java
@@ -1,14 +1,37 @@
 package io.gabo.schoolbridgeapi.domain;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity @Data @NoArgsConstructor
+@Entity @NoArgsConstructor
 @Table(name = "schools_education_levels")
 public class SchoolEducationLevel {
     @EmbeddedId private SchoolEducationLevelKey id;
 
     @ManyToOne(optional = false) @MapsId("schoolId") private School school;
     @ManyToOne(optional = false) @MapsId("educationLevelId") private EducationLevel educationLevel;
+
+    public SchoolEducationLevelKey getId() {
+        return id;
+    }
+
+    public void setId(SchoolEducationLevelKey id) {
+        this.id = id;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public EducationLevel getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(EducationLevel educationLevel) {
+        this.educationLevel = educationLevel;
+    }
 }
