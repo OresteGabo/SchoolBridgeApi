f079b27a0fc679eeb97c05fee8144bea50e78244
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:38:52 +0200

    Add Course entity definition

---
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Course.java
@@ -0,0 +1,33 @@
+package io.gabo.schoolbridgeapi.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+@Entity(name = "courses") @Data @NoArgsConstructor
+class Course {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+    @Column(length = 250)
+    private String description;
+}
+
+/*
+@Entity @Data @NoArgsConstructor
+class Course {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Column(nullable = false, length = 50, unique = true) private String name;
+    @Column(length = 250) private String description;
+
+    // reverse link for clarity
+    @ManyToMany(mappedBy = "mainCourses") private Set<Combination> combinations = new HashSet<>();
+
+}
+*/
