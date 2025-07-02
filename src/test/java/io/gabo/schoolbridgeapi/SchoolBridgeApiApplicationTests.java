21599ae2427bebdc4cba4f66f9ba206e1a3dd680
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 06:40:50 +0200

    Add SchoolBridgeApiApplicationTests class definition

---
+++ src/test/java/io/gabo/schoolbridgeapi/SchoolBridgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gabo.schoolbridgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBridgeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
