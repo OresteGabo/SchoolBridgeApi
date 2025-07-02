4a07375fd6543ef6154e39763a0f167548798034
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:31:10 +0200

    Add EducationLevelRepository with basic JpaRepository functionality

---
+++ src/main/java/io/gabo/schoolbridgeapi/repository/EducationLevelRepository.java
@@ -0,0 +1,8 @@
+package io.gabo.schoolbridgeapi.repository;
+
+import io.gabo.schoolbridgeapi.domain.EducationLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationLevelRepository extends JpaRepository<EducationLevel, Long> {
+}
+
