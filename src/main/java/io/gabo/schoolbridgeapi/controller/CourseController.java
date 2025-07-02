e7de971470199b1313de29ca68b2daa61ff59726
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 14:29:18 +0200

    Import `JsonIgnore` in CourseController to support future serialization adjustments

--- src/main/java/io/gabo/schoolbridgeapi/controller/CourseController.java
+++ src/main/java/io/gabo/schoolbridgeapi/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @RestController
 @RequestMapping("/api/courses")
