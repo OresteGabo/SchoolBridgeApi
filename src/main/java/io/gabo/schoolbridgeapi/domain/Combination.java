1c8c4d132cd74cf1106b6b992ebf30ff4a7e19d1
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 11:08:55 +0200

    Add Combination entity with relationships to Section and Course definitions

--- src/main/java/io/gabo/schoolbridgeapi/domain/Combination.java
+++ src/main/java/io/gabo/schoolbridgeapi/domain/Combination.java
@@ -4,19 +4,21 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "combinations")
-@Data @NoArgsConstructor
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity(name = "combinations") @Data @NoArgsConstructor
 class Combination {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Column(nullable = false, unique = true, length = 10) private String code; // e.g. "MCB"
+    @Column(length = 120) private String description;
 
-    @Column(nullable = false, unique = true, length = 10)
-    private String code;  // e.g., "MCB", "HEG"
+    @ManyToOne(optional = false) private Section section;
 
-    @Column(length = 100)
-    private String description; // optional: "Math-Chemistry-Biology"
-
-    @ManyToOne(optional = false)
-    private Section section; // e.g., Science, Arts
+    @ManyToMany
+    @JoinTable(name = "combination_courses",
+            joinColumns = @JoinColumn(name = "combination_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> mainCourses = new HashSet<>();
 }
-
