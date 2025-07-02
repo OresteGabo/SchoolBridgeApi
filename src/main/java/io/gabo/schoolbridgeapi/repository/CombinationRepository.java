d0db41fafcef68a1bb46a2530e95d941188bedf3
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-02 16:24:19 +0200

    Enhance CombinationRepository with `@EntityGraph` for optimized data fetching

--- src/main/java/io/gabo/schoolbridgeapi/repository/CombinationRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/CombinationRepository.java
@@ -1,11 +1,22 @@
 package io.gabo.schoolbridgeapi.repository;
 
 import io.gabo.schoolbridgeapi.domain.Combination;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CombinationRepository extends JpaRepository<Combination, Long> {
+
+    @EntityGraph(attributePaths = {"section.educationLevel.degreeType", "mainCourses"})
+    List<Combination> findAll();
+
+    @EntityGraph(attributePaths = {"section.educationLevel.degreeType", "mainCourses"})
+    Optional<Combination> findById(Long id);
+
+    @EntityGraph(attributePaths = {"section.educationLevel.degreeType", "mainCourses"})
     Optional<Combination> findByCodeIgnoreCase(String code);
 }
+
 
