e934dbbb1b118eaa802f4ac9fc620f3b6240cb97
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-08 01:45:20 +0200

    Refactor RoleRepository: rename `Role` to `UserRole` in interface and method for naming consistency

--- src/main/java/io/gabo/schoolbridgeapi/repository/RoleRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/RoleRepository.java
@@ -1,12 +1,12 @@
 package io.gabo.schoolbridgeapi.repository;
 
-import io.gabo.schoolbridgeapi.domain.Role;
+import io.gabo.schoolbridgeapi.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByDbNameIgnoreCase(String dbName);
+public interface RoleRepository extends JpaRepository<UserRole, Long> {
+    Optional<UserRole> findByDbNameIgnoreCase(String dbName);
 }
