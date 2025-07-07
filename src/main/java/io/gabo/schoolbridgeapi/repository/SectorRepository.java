e0d6f3583903c69df3cd32eb79e040530056e80f
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-08 01:49:46 +0200

    Add `findByNameIgnoreCase` method to SectorRepository for case-insensitive sector lookups

--- src/main/java/io/gabo/schoolbridgeapi/repository/SectorRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/SectorRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SectorRepository extends JpaRepository<Sector, Long> {
     List<Sector> findByDistrictId(Long districtId);
     List<Sector> findByDistrict(District district);
     int countByDistrictId(Long districtId);
+    Optional<Sector> findByNameIgnoreCase(String name);
 }
