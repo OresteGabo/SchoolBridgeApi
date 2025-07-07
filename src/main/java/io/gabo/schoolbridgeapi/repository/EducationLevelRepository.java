19f69b188fb10671fe6eaeefe86e7210d90eced2
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-08 01:44:37 +0200

    Add new query methods to EducationLevelRepository for promotion logic and degree type filtering

--- src/main/java/io/gabo/schoolbridgeapi/repository/EducationLevelRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/EducationLevelRepository.java
@@ -3,6 +3,14 @@ package io.gabo.schoolbridgeapi.repository;
 import io.gabo.schoolbridgeapi.domain.EducationLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EducationLevelRepository extends JpaRepository<EducationLevel, Long> {
-}
+import java.util.List;
+import java.util.Optional;
 
+public interface EducationLevelRepository extends JpaRepository<EducationLevel, Long> {
+
+    // Used for promotion logic
+    Optional<EducationLevel> findFirstByLevelOrderGreaterThanOrderByLevelOrderAsc(Integer currentOrder);
+
+    // For your controller
+    List<EducationLevel> findByDegreeType_NameIgnoreCaseOrderByLevelOrderAsc(String degreeTypeName);
+}
