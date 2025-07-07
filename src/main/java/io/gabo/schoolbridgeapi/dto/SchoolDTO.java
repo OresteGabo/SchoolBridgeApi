6c3fe87f93b65c6458ec674903910f8e738b56a6
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-08 01:46:36 +0200

    Add no-arg constructor and getter/setter methods to SchoolDTO for deserialization and improved field access

--- src/main/java/io/gabo/schoolbridgeapi/dto/SchoolDTO.java
+++ src/main/java/io/gabo/schoolbridgeapi/dto/SchoolDTO.java
@@ -14,6 +14,8 @@ public class SchoolDTO {
     private String districtName;
     private String sectorName;
 
+    public SchoolDTO() {} // for deserialization
+
     public SchoolDTO(School school) {
         this.id = school.getId();
         this.name = school.getName();
@@ -25,5 +27,76 @@ public class SchoolDTO {
         this.districtName = school.getDistrict().getName();
         this.sectorName = school.getSector().getName();
     }
-}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbrevName() {
+        return abbrevName;
+    }
+
+    public void setAbbrevName(String abbrevName) {
+        this.abbrevName = abbrevName;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getSectorName() {
+        return sectorName;
+    }
+
+    public void setSectorName(String sectorName) {
+        this.sectorName = sectorName;
+    }
+}
