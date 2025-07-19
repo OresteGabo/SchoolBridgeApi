4e3a5b59d0d647e6eddb1a3bf73a128160600837
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-19 05:41:09 +0200

    Update AuthService to integrate JWTService for token generation and refine user authentication logic

--- src/main/java/io/gabo/schoolbridgeapi/service/AuthService.java
+++ src/main/java/io/gabo/schoolbridgeapi/service/AuthService.java
@@ -7,6 +7,7 @@ import io.gabo.schoolbridgeapi.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -17,15 +18,17 @@ public class AuthService {
 
     private final UserRepository userRepo;
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final JWTService jwtService;
 
-    public AuthService(UserRepository userRepo) {
+    public AuthService(UserRepository userRepo, JWTService jwtService) {
         this.userRepo = userRepo;
+        this.jwtService = jwtService;
     }
 
-    public LoginResponseDTO authenticateUser(String username, String password) {
+    public LoginResponseDTO authenticateUser(String username, String password) throws NoSuchAlgorithmException {
         System.out.println("Attempting login for: " + username);
 
-        Optional<User> userOpt = userRepo.findByUserName(username)
+        Optional<User> userOpt = userRepo.findByUsername(username)
                 .or(() -> userRepo.findByEmail(username));
         System.out.println("user opt = "+userOpt);
         if (userOpt.isEmpty()) {
@@ -34,22 +37,22 @@ public class AuthService {
         }
 
         User user = userOpt.get();
-        if (!encoder.matches(password, user.getPasswordHash())) {
+        if (!encoder.matches(password, user.getPassword())) {
             System.out.println("❌ Password mismatch for: " + username);
             throw new RuntimeException("Invalid username or password.");
         }
 
-        System.out.println("✅ Authenticated user: " + user.getUserName());
+        System.out.println("✅ Authenticated user: " + user.getUsername());
 
         Set<String> activeRoles = user.getRoles().stream()
                 .map(UserRole::getDbName)
                 .collect(Collectors.toSet());
 
-        String fullName = user.getFirstName() + " " + user.getFamilyName();
+        //String fullName = user.getFirstName() + " " + user.getFamilyName();
         String joinDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMM yyyy"));
 
         return LoginResponseDTO.builder()
-                .authToken(UUID.randomUUID().toString())
+                .authToken(jwtService.generateToken(user.getUsername()))
                 .refreshToken(UUID.randomUUID().toString())
                 .userId("usr_" + user.getId())
                 .email(user.getEmail())
