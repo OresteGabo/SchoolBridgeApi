b35ea82683cc9871eb3d256a7e652f2c72db8438
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-19 05:41:20 +0200

    Update SecurityConfig to add JwtFilter, configure stateless sessions, and permit access to register endpoint

--- src/main/java/io/gabo/schoolbridgeapi/config/SecurityConfig.java
+++ src/main/java/io/gabo/schoolbridgeapi/config/SecurityConfig.java
@@ -1,23 +1,33 @@
 package io.gabo.schoolbridgeapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // ✅ CSRF disabled (for mobile APIs)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll() // ✅ Allow unauthenticated access
+                .csrf(AbstractHttpConfigurer::disable) // ✅ CSRF disabled (for mobile APIs)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/auth/login","/api/auth/register")
+                        .permitAll() // ✅ Allow unauthenticated access
                         .anyRequest().authenticated()                // 🔐 Secure everything else
                 )
-                .httpBasic(Customizer.withDefaults()); // Optional (enables browser login test)
+                .httpBasic(Customizer.withDefaults()) // Optional (enables browser login test)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
