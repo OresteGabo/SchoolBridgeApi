51d5a1faa041cf6ab050bb1c1ce60713d1093e65
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2025-07-19 05:41:47 +0200

    Annotate `UserRepository` with `@Repository` and update method signature to use `username` instead of `userName`

--- src/main/java/io/gabo/schoolbridgeapi/repository/UserRepository.java
+++ src/main/java/io/gabo/schoolbridgeapi/repository/UserRepository.java
@@ -2,12 +2,11 @@ package io.gabo.schoolbridgeapi.repository;
 
 import io.gabo.schoolbridgeapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUserName(String userName);
-
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
